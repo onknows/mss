86e08e8bc66f69e444d23edc3a5e7a239b7c5107
Onno van der Straaten <onno.van.der.straaten@onknows.com>
2017-03-27 13:19:37 +0200

    SQLite not MySQL

--- app.rb
+++ app.rb
@@ -5,8 +5,8 @@ require 'digest/md5'
 require 'rack-flash'
 
 ActiveRecord::Base.establish_connection(
-  :adapter  => "sqlite",
-  :database => "db/tms.sqlite"
+  adapter: 'sqlite3',
+  database: 'db/tms.sqlite'
 )
 
 class Mailbox < ActiveRecord::Base
@@ -18,68 +18,63 @@ end
 
 class App < Sinatra::Application
 end
-  enable :sessions
-  use Rack::Flash
+enable :sessions
+use Rack::Flash
 
-  get '/' do
-    redirect "/mss/", 303
+get '/' do
+  redirect '/mss/', 303
+end
+
+get '/mss/' do
+  @domains = Domain.all
+  erb :domains
+end
+
+put '/mss/' do
+  Domain.create(fqdn: params['fqdn'], type: 'local',
+                description: params['description'] + '. ' + (ENV['REMOTE_USER'] || ''),
+                active: 1)
+  flash[:notice] = "Domain #{params['fqdn']} created!"
+  redirect '/mss/', 302
+end
+
+get '/mss/:domain/' do |n|
+  @domain = Domain.find_by fqdn: n
+  @mailboxes = Mailbox.where domain_id: @domain.id
+  erb :domain
+end
+
+put '/mss/:domain/' do |n|
+  domain = Domain.find_by fqdn: n
+  mailbox = Mailbox.create(domain_id: domain.id, local_part: params['local_part'],
+                           password: params['password'],
+                           description: params['description'] + '. ' + (ENV['REMOTE_USER'] || ''),
+                           active: 1)
+  flash[:notice] = "Mailbox #{mailbox.local_part}@#{domain.fqdn} created!"
+  redirect "/mss/#{domain.fqdn}/", 302
+end
+
+get '/mss/:domain/:local_part/' do
+  @domain = Domain.find_by fqdn: params['domain']
+  @mailbox = Mailbox.find_by domain_id: @domain.id, local_part: params['local_part']
+  erb :mailbox
+end
+
+delete '/mss/:domain/:local_part/' do
+  domain = Domain.find_by fqdn: params['domain']
+  mailbox = Mailbox.find_by domain_id: domain.id, local_part: params['local_part']
+  mailbox.delete
+  flash[:notice] = "Mailbox #{mailbox.local_part}@#{domain.fqdn} deleted!"
+  redirect "/mss/#{domain.fqdn}/", 302
+end
+
+get '/mss/:domain/:local_part/verify-password/' do
+  domain = Domain.find_by fqdn: params['domain']
+  mailbox = Mailbox.find_by domain_id: domain.id, local_part: params['local_part']
+  if params['domain'] == mailbox.password
+    flash[:notice] = 'Verified! Password is correct.'
+  else
+    flash[:notice] = '<font color="red">Password is not correct!</font>'
   end
-
-  get '/mss/' do
-    @domains = Domain.all
-    erb :domains
-  end
-
-  put '/mss/' do
-    Domain.create(fqdn: params['fqdn'], type: 'local',
-      description: params['description'] + ". " + (ENV['REMOTE_USER'] || ''),
-      active: 1)
-    flash[:notice] = "Domain #{params['fqdn']} created!"
-    redirect "/mss/", 302
-  end
-
-  get '/mss/:domain/' do |n|
-    @domain = Domain.find_by fqdn: n
-    @mailboxes = Mailbox.where domain_id: @domain.id
-    erb :domain
-  end
-
-  put '/mss/:domain/' do |n|
-    domain = Domain.find_by fqdn: n
-    mailbox = Mailbox.create(domain_id: domain.id, local_part: params['local_part'],
-      password: params['password'],
-      description: params['description'] + ". " + (ENV['REMOTE_USER'] || ''),
-      active: 1)
-    flash[:notice] = "Mailbox #{mailbox.local_part}@#{domain.fqdn} created!"
-    redirect "/mss/#{domain.fqdn}/", 302
-  end
-
-  get '/mss/:domain/:local_part/' do 
-    @domain = Domain.find_by fqdn: params['domain']
-    @mailbox = Mailbox.find_by domain_id: @domain.id, local_part: params['local_part']
-    erb :mailbox
-  end
-
-  delete '/mss/:domain/:local_part/' do 
-    domain = Domain.find_by fqdn: params['domain']
-    mailbox = Mailbox.find_by domain_id: domain.id, local_part: params['local_part']
-    mailbox.delete
-    flash[:notice] = "Mailbox #{mailbox.local_part}@#{domain.fqdn} deleted!"
-    redirect "/mss/#{domain.fqdn}/", 302  
-  end
-
-  get '/mss/:domain/:local_part/verify-password/' do 
-    domain = Domain.find_by fqdn: params['domain']
-    mailbox = Mailbox.find_by domain_id: domain.id, local_part: params['local_part']
-    if params['domain'] == mailbox.password
-      flash[:notice] = "Verified! Password is correct."      
-    else
-      flash[:notice] = "<font color=\"red\">Password is not correct!</font>"           
-    end    
-    redirect "/mss/#{params['domain']}/#{params['local_part']}/", 302
-  end  
-
-
-
-
-
+  redirect "/mss/#{params['domain']}/#{params['local_part']}/", 302
+end
